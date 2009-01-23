267afd714f8bf40690b597c6fb6603782252681f
Fabian Steeg <fsteeg@gmail.com>
2009-01-23 10:41:07 +0000

    Added stack overflow script (so.rb).

--- com.quui.chat.irc/src-ruby/so.rb
+++ com.quui.chat.irc/src-ruby/so.rb
@@ -7,6 +7,7 @@ def init
 end
 def doSo(incoming)
   adress = "http://stackoverflow.com"
+  regex = /<h3><a href="([^"]+)"[^>]*>([^<]+)<\/a><\/h3>/m
   if (incoming.strip=='')
    adress = adress#"http://stackoverflow.com"#return ["Usage: so (<tags>)",nil]
   else
@@ -14,12 +15,20 @@ def doSo(incoming)
     adress = adress<<"/questions/tagged/#{text}"
    end
     res = WebsiteLookup.new(adress,"UTF-8").text
-   	regex = /<h3><a href="([^"]+)"[^>]*>([^<]+)<\/a><\/h3>/m
+   	
     all=''
     res.scan regex do |url|
       all << "#" << CGI.unescapeHTML(url.reverse.join(": http://stackoverflow.com").strip.gsub(/<([^>]+)>/,""))
   end
   p "All: " << all
+  if(all.strip=='')
+  	adress = "http://stackoverflow.com/search?q=#{text}"
+	 res = WebsiteLookup.new(adress,"UTF-8").text
+	 all=''
+    res.scan regex do |url|
+      all << "#" << CGI.unescapeHTML(url.reverse.join(": http://stackoverflow.com").strip.gsub(/<([^>]+)>/,""))
+	  end
+  end
   if(all.strip=='')
   	all = 'No idea!'
   end
