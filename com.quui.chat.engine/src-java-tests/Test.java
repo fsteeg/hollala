f1e70bdb58718285c437abb54d10f5cc07ad0e98
Fabian Steeg <fsteeg@gmail.com>
2009-02-21 20:08:21 +0000

    Cleanup

--- com.quui.chat.engine/src-java-tests/Test.java
+++ com.quui.chat.engine/src-java-tests/Test.java
@@ -1,11 +1,22 @@
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
 
 public class Test {
 
     /**
      * @param args
+     * @throws MalformedURLException 
+     * @throws URISyntaxException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException, URISyntaxException {
+        String escapeHtml = StringEscapeUtils.escapeHtml("http://www.google.com?q=a b");
+        System.out.println(new URL("http://www.google.com?q=a b").toURI());
+        
+        
     // TODO Auto-generated method stub
         String string = "Company A, 123 Any Street Suite 200, Anytown, AK 99012";
         String string2 = "CA, 123 Any Street Ste 200, Anytown, AK 99012";
